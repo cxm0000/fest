54058e44684c8dd141a29eb10a596c7a9a17253b
Xiaoming Cai(Ming) <ming.cxm@gmail.com>
2015-10-12 22:50:11 +0200

    changed include paths to be relative

--- index.php
+++ index.php
@@ -6,18 +6,11 @@
         <meta name="msapplication-tap-highlight" content="no" />
         <!-- WARNING: for iOS 7, remove the width=device-width and height=device-height attributes. See https://issues.apache.org/jira/browse/CB-4323 -->
         <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1" />
-        <link rel="stylesheet" type="text/css" href="css/index.css" />
-        <title>Barnkalas App</title>
-
-
-        <script src="https://fb.me/JSXTransformer-0.13.3.js"></script>
-
-        <script src="js/libs/require.js"></script>
-		<script src="js/app/bootstrap.js"></script>
-
+        <title>M&S - Inflyttningsfest</title>
+        <script src="../js/libs/require.js"></script>
+		<script src="../js/app/bootstrap.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/survey.css">
-
+		<link rel="stylesheet" href="../css/survey.css">
 
     </head>
     <body>
