f94e1176844f63009f1113e4c44bf15e5a9591cf
Xiaoming Cai(Ming) <ming.cxm@gmail.com>
2015-10-12 22:58:51 +0200

    changed the file path just for this branch

--- js/app/bootstrap.js
+++ js/app/bootstrap.js
@@ -1,7 +1,7 @@
 require.config({
-    baseUrl: '../js/libs',
+    baseUrl: '/fest/js/libs',
     paths: {
-        "app": "../app",
+        "app": "/fest/app",
         "react": "//fb.me/react-0.13.3",
         "parse": "//www.parsecdn.com/js/parse-1.6.5.min",
         "jquery": '//code.jquery.com/jquery-2.1.4.min',
